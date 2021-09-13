f644b469d9206a496d42bbad5c8d0ed893f438c1
Anton Fladvad <antonzilla@hotmail.com>
2021-09-13 20:19:32 +0200

    added really-cat

---
+++ dbconfig.js
@@ -0,0 +1,10 @@
+module.exports = {
+    user: 'ysbibvle',
+    host: 'hattie.db.elephantsql.com',
+    database: 'ysbibvle',
+    password: '7wD-LX4KUqbOCF9XOiHe4Q4xKRt0VZaE',
+    port: 5432
+}
+
+
+//postgres://ysbibvle:7wD-LX4KUqbOCF9XOiHe4Q4xKRt0VZaE@hattie.db.elephantsql.com:5432/ysbibvle
